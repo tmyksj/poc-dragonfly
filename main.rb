22d9e4ed981da7d9701d5c50b51d092ea64785d8
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-07-25 07:55:53 +0900

    Write multi operation benchmark

--- main.rb
+++ main.rb
@@ -12,51 +12,87 @@ connections = {
 
 # Benchmark properties
 trials = 3
-kv_size = 10000
+kv_size = {
+  s: 10000,
+  m: 10000,
+}
 
 # Benchmark results
 results = connections.map { |k, _|
-  [k, { set: [], get: [], del: [] }]
+  [k, { set: [], get: [], del: [], mset: [], mget: [], mdel: [] }]
 }.to_h
 
-# Benchmark
-trials.times do
+# Benchmark (Single Ops)
+trials.times do |trial|
   # Key Values
-  kvs = Array.new(kv_size, &:itself).shuffle.map { |idx| ["Key #{idx}", "Val #{idx}"] }.to_h
+  kvs = Array.new(kv_size[:s], &:itself).shuffle.map { |idx| ["Key #{idx}", "Val #{idx}"] }.to_h
 
-  # Set
   connections.each do |server, connection|
+    # Set
+    $stderr.puts(format("[INFO] %d / %d: %9s Set", trial + 1, trials, server))
     results[server][:set] << Benchmark.realtime do
       kvs.each do |key, value|
         connection.set(key, value)
       end
     end
-  end
 
-  # Get
-  connections.each do |server, connection|
+    # Get
+    $stderr.puts(format("[INFO] %d / %d: %9s Get", trial + 1, trials, server))
     results[server][:get] << Benchmark.realtime do
       kvs.each do |key, _|
         connection.get(key)
       end
     end
-  end
 
-  # Del
-  connections.each do |server, connection|
+    # Del
+    $stderr.puts(format("[INFO] %d / %d: %9s Del", trial + 1, trials, server))
     results[server][:del] << Benchmark.realtime do
       kvs.each do |key, _|
         connection.del(key)
       end
     end
+  rescue => e
+    $stderr.puts(format("[ERRO] %d / %d: %9s %s", trial + 1, trials, server, e))
+  end
+end
+
+# Benchmark (Multi Ops)
+trials.times do |trial|
+  # Key Values
+  kvs = Array.new(kv_size[:m], &:itself).shuffle.map { |idx| ["Key #{idx}", "Val #{idx}"] }.to_h
+
+  connections.each do |server, connection|
+    # Multi Set
+    $stderr.puts(format("[INFO] %d / %d: %9s Multi Set", trial + 1, trials, server))
+    results[server][:mset] << Benchmark.realtime do
+      connection.mset(*kvs.to_a.flatten)
+    end
+
+    # Multi Get
+    $stderr.puts(format("[INFO] %d / %d: %9s Multi Get", trial + 1, trials, server))
+    results[server][:mget] << Benchmark.realtime do
+      connection.mget(*kvs.keys)
+    end
+
+    # Multi Del
+    $stderr.puts(format("[INFO] %d / %d: %9s Multi Del", trial + 1, trials, server))
+    results[server][:mdel] << Benchmark.realtime do
+      connection.del(*kvs.keys)
+    end
+  rescue => e
+    $stderr.puts(format("[ERRO] %d / %d: %9s %s", trial + 1, trials, server, e))
   end
 end
 
 # Output
 results.each do |server, result|
-  puts "#{server} #{'-' * 80}"[0..80]
+  puts("#{server} #{'-' * 80}"[0..80])
 
   result.each do |op, reals|
-    puts(format("%s: AVG = %.6f, All = [#{reals.map { |_| "%.6f" }.join(", ")}]", op, reals.sum / reals.size, *reals))
+    if reals.empty?
+      puts(format("%4s: AVG = , All = []", op))
+    else
+      puts(format("%4s: AVG = %.6f, All = [#{reals.map { |_| "%.6f" }.join(", ")}]", op, reals.sum / reals.size, *reals))
+    end
   end
 end
